ca1005a229088ea8e486c003c0d554aaaa9cf98e
iMac <imac@iMacs-iMac.local>
2022-05-18 14:34:34 +0530

    setup project

---
+++ SamplePipelineProj/SamplePipelineProj/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SamplePipelineProj
+//
+//  Created by iMac on 18/05/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
